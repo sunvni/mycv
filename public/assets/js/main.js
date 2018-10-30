d88c260a40745a3b31a9f0eb28e7bedff82fbde4
Hoang Hieu <sunvni@gmail.com>
2018-10-30 12:55:15 +0900

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,27 @@
-window.addEventListener('load', ()=> {
-    $link = document.querySelectorAll('.nav a');
-    $link.forEach(element => {
-        element.addEventListener('click', (e)=> {
-            $link.forEach(es => {
-                es.classList.remove('active');
-            });
-            e.target.classList.add('active');
-        })
+
+/* window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.2'
     });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk')); */
+
+function showMenu()
+{
+    $navbar = document.querySelector("#navbar");
+    $navbar.classList.toggle('responsive');
+}
+
+window.addEventListener('load', () => {
+    //FB.CustomerChat.show(true);
 })
