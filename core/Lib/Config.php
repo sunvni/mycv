87b4a7e61bc1313e0c8ea2c308f98fc099864dca
Hoang Hieu <sunvni@gmail.com>
2018-11-13 23:20:39 +0900

    update

--- core/Lib/Config.php
+++ core/Lib/Config.php
@@ -3,20 +3,4 @@ namespace Core\Lib;
 
 class Config extends MRObject
 {
-    private $data;
-
-    public function __construct($data)
-    {
-        $this->data = $data;
-    }
-
-    public function __get($name)
-    {
-        return $this->data[$name];
-    }
-
-    public function get()
-    {
-        return $this->data;
-    }
 }
